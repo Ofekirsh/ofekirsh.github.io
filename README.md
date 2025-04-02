ae76c4e5e05235fc2852bc0b18a0de775e2639ec
Rami Hashan <dxroomram@gmail.com>
2025-04-02 19:31:16 +0300

    transferring to react

--- README.md
+++
@@ -1,1 +0,0 @@
-# ofekirsh.github.io
--- navbar/navbar-loader.js
+++
@@ -1,7 +0,0 @@
-// Load navbar from separate file
-
-fetch("/navbar/navbar.html")
-  .then((res) => res.text())
-  .then((html) => {
-    document.getElementById("navbar-container").innerHTML = html;
-  });
